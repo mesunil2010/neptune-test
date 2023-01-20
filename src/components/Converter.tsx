0c662b0dab0e648ae9120d93208098858c718586
sunil chaulagain <mesunil2010@gmail.com>
2023-01-20 07:04:46 +0545

    Released currency converter feature

---
+++ src/components/Converter.tsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Form from "react-bootstrap/Form";
+
+const Converter = () => {
+    const CHANGE_RATE = 3
+    const [nep, setNep] = useState<number>(0)
+    const [busd, setBusd] = useState<number>(0)
+
+    const formatNumber = (value: number) => value.toFixed(2) as unknown as number;
+
+    const onNepChangeHandle = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = Number(e.target.value)
+
+        setNep((value))
+        setBusd(formatNumber(value * CHANGE_RATE))
+    }
+
+    const onBusdChangeHandle = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = Number(e.target.value)
+
+        setNep(formatNumber(value / CHANGE_RATE))
+        setBusd((value))
+    }
+
+    return (
+        <Card style={{ width: "480px" }} className="p-4">
+            <Card.Body>
+                <Card.Title>Crypto Converter</Card.Title>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>NEP</Form.Label>
+                            <Form.Control type="text" placeholder="0.00" onChange={onNepChangeHandle} value={nep}/>
+                            <Form.Text className="text-muted" ></Form.Text>
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>BUSD</Form.Label>
+                            <Form.Control type="text" placeholder="0.00" onChange={onBusdChangeHandle} value={busd}/>
+                        </Form.Group>
+                    </Form>
+                <Button variant="primary" className="text-center">
+                    Connect
+                </Button>
+            </Card.Body>
+        </Card>
+    );
+};
+
+export default Converter;
